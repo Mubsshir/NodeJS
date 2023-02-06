5c3c97bce81add9d201bd6f1683492345a13a991
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2023-02-06 17:36:23 +0530

    Take a response from user and write it on the file on our local filesystem

--- Basics/app.js
+++ Basics/app.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const file = require('fs');
 const html = "<html><head><title>Hello world</title></head><body><h1>Hello from my NodeJS Server.</h1></body></html>";
 const rqListner = (req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
     res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html><head><title>Hello world</title></head><body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body></html>');
+        return res.end();
+    }
+    if (url === "/message" && req.method === "POST") {
+        const data = [];
+        req.on('data', (chunk) => {
+            data.push(chunk);
+        });
+        req.on('end', () => {
+            const parseBody = Buffer.concat(data).toString();
+            const message = (parseBody.split('=')[1]);
+            file.writeFileSync('message.txt', message);
+        });
+        return res.end();
+    }
     res.write(html);
     res.end();
 };
-
 const server = http.createServer(rqListner);
-
 server.listen(3000);
