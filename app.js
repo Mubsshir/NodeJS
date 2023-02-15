97ee6b99474fdf7f8efb7fd2da4d5cfe0b6950cf
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2023-02-15 23:12:48 +0530

    install express.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const {reqHandler} =require('./Npm scripts/reqHandler')
 const server = http.createServer(reqHandler);
-server.listen(3000);
+server.relisten(3000);
 console.log("Listening for requests");
