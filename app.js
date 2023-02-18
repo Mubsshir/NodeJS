7b1b54c2520329bc002f1325bc5cdb36fdc1abe5
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2023-02-18 23:01:57 +0530

    learnt how to use app.get and body parser

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const http = require("http");
-const express=require("express")
-const server = http.createServer(reqHandler);
-server.relisten(3000);
+const express=require("express");
+const bodyParser=require('body-parser');
+const app =express();
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.post('/message',(req,res,next)=>{
+    console.log("Message page sent");
+    const body=req.body;
+    console.log(body.message)
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    res.sendFile('E:\\node\\index.html');
+    console.log("Home page sent");
+})
+app.listen(3000);
 console.log("Listening for requests");
