13540753e93d0209b1ae4a50d598ad60d76b13c0
mubsshir <khanmubsshir@gmail.com>
2023-02-21 16:45:56 +0530

    Merge All

--- Basics/app.js
+++ Basics/app.js
@@ -1,7 +1,10 @@
-const { Console } = require("console");
 const http = require("http");
-const {reqHandler} =require('./reqListner')
-const server = http.createServer(reqHandler);
+const app =require("express.js");
+
+app.use((req,res,next)=>{
+    console.log("I ran")
+})
+const server = http.createServer(app);
 server.listen(3000);
 console.log("Listening for requests");
 
--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-const http = require("http");
-const express=require("express")
-const server = http.createServer(reqHandler);
-server.relisten(3000);
+const express =require("express");
+const app=express();
+
+app.use('/',(req,res,next)=>{
+    res.send("<h2>Home page of ExpressJS</h2>")
+    next();
+})
+app.use('/message',(req,res,next)=>{
+    res.send('<h1>hello from ExpressJS</h1>')
+    next();
+})
+app.listen(3000)
 console.log("Listening for requests");
+
